da0d3feb06406c3ff5406be3e22efdd91a9e3d82
Piki Templates <59523212+pikitemplates@users.noreply.github.com>
2022-11-10 13:25:53 +0530

    Create loc.js

---
+++ loc.js
@@ -0,0 +1,6 @@
+$(window).one("scroll", (function() {
+	$.get("ip-api.com/json/", function (response) {
+        var country = response.countryCode;
+        if(country == 'IN') document.getElementById(country).style.display = "block";
+	}, "jsonp");
+}));
