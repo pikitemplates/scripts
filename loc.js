bba861025efb2723b676cce8d3719d8724b3f7ed
Piki Templates <59523212+pikitemplates@users.noreply.github.com>
2022-11-10 13:27:20 +0530

    Update loc.js

--- loc.js
+++ loc.js
@@ -1,5 +1,5 @@
 $(window).one("scroll", (function() {
-	$.get("ip-api.com/json/", function (response) {
+	$.get("http://ip-api.com/json/", function (response) {
         var country = response.countryCode;
         if(country == 'IN') document.getElementById(country).style.display = "block";
 	}, "jsonp");
